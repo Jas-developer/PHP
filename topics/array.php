00cabd432dc80bcea4eecc2d8531aeafaeea7309
Jason <devop.jas@gmail.com>
2025-05-29 10:02:58 +0800

    Creating array

---
+++ topics/array.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+<?php
+
+//array in php 
+
+$cars = array("volvo","bmw","toyota");
+
+//working with arrays or use case of array s
+
+
+
+$myArr = array("Volvo",15,["apple","bananas"], 4.4);
+
+
+
+
+
+
+
+//create arrays
+//access arrays
+//update arrays
+//add arrays items
+//remove array items 
+//sort arrays 
+
+
+
+
+echo $myArr[0];
+
+
+$cars = array("honda","suzuki","ferrari");
+
+foreach ( $cars as $x) {
+   echo "$x <br>";
+}
+
+
+
+
+?>
+    
+</body>
+</html>
